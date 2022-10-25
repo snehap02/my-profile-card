9c2cd34e9b83ea4ac0d96f0968173497404f6a1e
Sneha Purkayastha <iamsneha0296@gmail.com>
2022-10-25 15:35:29 +0530

    profile card

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        body: '#E3E4ED',
+        div: '#E6E7EE'
+      },
+      fontFamily:{
+        firstfont: 'Montserrat',
+        secondfont: 'Poppins'
+      },
+      width:{
+        firstwidth: '8.4rem'
+      }
+    },
+  },
+  plugins: [],
+}
